f74d3b5d51adc2d7c4dfb7f817fe83ce1d21e451
ManhAnNguyen <thungantn1999@gmail.com>
2022-07-03 20:14:09 +0700

    day8

--- CI74/src/App.js
+++ CI74/src/App.js
@@ -1,15 +1,22 @@
-import React from "react";
-import Ex1 from "./Home/Ex1";
-import Ex2 from "./Home/Ex2";
-
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React, { useState } from "react";
 
 const App = () => {
+  const [products, setProducts] = useState(null);
+
+  const fetchData = async () => {
+    const response = await fetch(`https://fakestoreapi.com/products`);
+    const data = await response.json();
+
+    setProducts(data);
+  };
+
+  console.log(products);
+
   return (
     <div>
-      <Ex2 />
-      <ToastContainer position="top-left" theme="dark" />
+      <button onClick={fetchData}>Call Api</button>
+      {products &&
+        products.map((product, index) => <h1 key={index}>{product.title}</h1>)}
     </div>
   );
 };
